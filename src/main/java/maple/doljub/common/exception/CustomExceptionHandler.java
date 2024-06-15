e5093249d80e1cda77fb156e128370960be8f9e8
fprh13 <fprh13@gmail.com>
2024-06-15 16:07:35 +0900

    Feat: 에러 페이지

--- src/main/java/maple/doljub/common/exception/CustomExceptionHandler.java
+++ src/main/java/maple/doljub/common/exception/CustomExceptionHandler.java
@@ -1,27 +1,28 @@
-package maple.doljub.common.exception;
-
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-@ControllerAdvice
-public class CustomExceptionHandler {
-
-    /**
-     * exception
-     */
-    @ExceptionHandler(CustomException.class)
-    protected ResponseEntity<CustomErrorResponseDto> handleCustomException(CustomException e) {
-        return CustomErrorResponseDto.fail(e.getErrorCode());
-    }
-
-    /**
-     * validation
-     */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    ResponseEntity<CustomErrorResponseDto> handleValidationException(MethodArgumentNotValidException e) {
-        return CustomErrorResponseDto.valid(e);
-    }
-}
+//package maple.doljub.common.exception;
+//
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@ControllerAdvice
+//public class CustomExceptionHandler {
+//
+//    // CustomException에 대한 처리
+//    @ExceptionHandler(CustomException.class)
+//    protected ModelAndView handleCustomException(CustomException e) {
+//        ModelAndView modelAndView = new ModelAndView("error");
+//        modelAndView.addObject("errorMessage", e.getMessage());
+//        return modelAndView;
+//    }
+//
+//    // MethodArgumentNotValidException에 대한 처리
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    protected ModelAndView handleValidationException(MethodArgumentNotValidException e) {
+//        ModelAndView modelAndView = new ModelAndView("validation_error");
+//        modelAndView.addObject("errorMessage", "Validation failed");
+//        return modelAndView;
+//    }
+//}
